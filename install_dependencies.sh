17b7e715210676fc4a67fac6c29df98dc4ae45f9
edwin <edwin@mediamonks.com>
2017-12-07 10:40:43 +0100

    Package installation logic based on php version

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install make build-essential automake php5-dev php-pear
+sudo apt-get install make build-essential automake php-pear
 
 if [[ "$TRAVIS_PHP_VERSION" < "7.2" ]]; then
     git clone git://github.com/jedisct1/libsodium.git
@@ -18,8 +18,6 @@ pecl channel-update pecl.php.net
 
 if [[ "$TRAVIS_PHP_VERSION" == "5.6" ]]; then
     pecl install libsodium-1.0.7
-fi;
-
-if [[ "$TRAVIS_PHP_VERSION" > "5.6" ]] && [[ "$TRAVIS_PHP_VERSION" < "7.2" ]]; then
+elif [[ "$TRAVIS_PHP_VERSION" < "7.2" ]]; then
     pecl install libsodium
 fi;
