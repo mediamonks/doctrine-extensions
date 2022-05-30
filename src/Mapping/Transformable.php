63b8a44a3b55f371b62abb10025ac14bb40ad6a9
edwin <edwin@mediamonks.com>
2022-05-30 20:00:57 +0200

    Implement internal Gedmo\Mapping\Annotation\Annotation interface.

--- src/Mapping/Transformable.php
+++ src/Mapping/Transformable.php
@@ -3,13 +3,14 @@
 namespace MediaMonks\Doctrine\Mapping;
 
 use Attribute;
+use Doctrine\ORM\Mapping\Annotation;
 
 /**
  * @Annotation
  * @Target("PROPERTY")
  */
 #[Attribute(Attribute::TARGET_PROPERTY)]
-final class Transformable
+final class Transformable implements Annotation
 {
     public function __construct(array $data = [], public string $name = 'noop')
     {
