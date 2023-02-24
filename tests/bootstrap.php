1ff0eef97687a74af0f3cc12bfd5e753dc33013e
Marco Brotas <arkightcrossfaith@gmail.com>
2023-02-24 14:53:52 +0100

    Fix to TransformerPool to handle the PHP 8 deprecation warning (#14)
    * Fix to TransformerPool to handle the PHP 8 deprecation warning
    
    * Removes AnnotationRegistry::registerLoader from unit tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,20 +1,17 @@
 <?php
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
+use Composer\Autoload\ClassLoader;
 use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\Common\Annotations\PsrCachedReader;
-use Composer\Autoload\ClassLoader;
 use Symfony\Component\Cache\Adapter\ArrayAdapter as ArrayAdapterAlias;
 
-define('TESTS_TEMP_DIR', sys_get_temp_dir().'/doctrine-extension-tests');
+define('TESTS_TEMP_DIR', sys_get_temp_dir() . '/doctrine-extension-tests');
 
 /** @var $loader ClassLoader */
 $loader = require __DIR__ . '/../vendor/autoload.php';
 $loader->add('Tool', __DIR__ . '/../vendor/gedmo/doctrine-extensions/tests/Gedmo/Tool');
 $loader->add('Transformable\\Fixture', __DIR__ . '/src');
 
-AnnotationRegistry::registerLoader([$loader, 'loadClass']);
-
-$reader                    = new AnnotationReader();
-$reader                    = new PsrCachedReader($reader, new ArrayAdapterAlias());
+$reader = new AnnotationReader();
+$reader = new PsrCachedReader($reader, new ArrayAdapterAlias());
 $_ENV['annotation_reader'] = $reader;
